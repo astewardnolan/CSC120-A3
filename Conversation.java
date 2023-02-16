c40e1b9aa789a705b6ce298007e054c586f6fe55
Ashby Steward-Nolan <astewardnolan@smith.edugit config --global user.email astewardnolan@smith.edu>
2023-02-15 23:38:28 -0500

    Done A3

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,109 @@
+import java.util.Scanner;
+import javax.sound.sampled.SourceDataLine;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.*;
 class Conversation {
 
   public static void main(String[] arguments) {
     // You will start the conversation here.
-  }
-}
+    
+   
+    // asks user how many rounds to play
+    System.out.println("how many rounds");
+    Scanner input= new Scanner (System.in);
+    double rounds= input.nextDouble();
+    Scanner input1;
+    String response;
+    Random r= new Random();
+
+  // list of statements and resposnes for transcript
+    
+  ArrayList<String> transcript = new ArrayList<String>();
+  
+// print greeting,  
+
+    System.out.println("Hey! Hows it going?");
+
+  //list of random responses for else statement
+    ArrayList<String> rand_response = new ArrayList<String>();
+    rand_response.add("wow");
+    rand_response.add("really?");
+    rand_response.add("interesting...");
+    rand_response.add("so cool!");
+    
+    
+    //random method loop
+    /* method to randomly select index from Arraylist rand_resposne
+     * 
+     */
+     Random random_method = new Random(); {
+      for (int i = 0; i < rand_response.size(); i++)
+      {
+        int index = random_method.nextInt(rand_response.size());
+      }
+
+    /*For loop depending on amount of rounds entered by user
+     * 
+     */
+    for (int i=0; i<= rounds; i++ ){
+      String statement = input.next(); 
+      transcript.add(statement);
+      
+      //need to split the sentence
+      String str[] = statement.split(" ");
+  /* for loop depedning on amount of words in statment
+   * 
+   */
+      for (int j=0; j<str.length; j++){ 
+        
+        String word;
+        word= str[j];
+        if(word.equals("I")){
+          // trying to replace i with updated word
+          word= ("You");}
+        else if (word.equals("me")){
+            // trying to replace i with updated word
+          word= ("you");}
+        else if (word.equals("you")){
+            // trying to replace i with updated word
+          word= ("me");}
+        else if(word.equals("am")){
+            // trying to replace i with updated word
+          word= ("are");}
+        else if(word.equals("I")){
+            // trying to replace i with updated word
+          word= ("You");}
+        else if(word.equals("my")){
+            // trying to replace i with updated word
+          word= ("Your");}
+        else if(word.equals("your")){
+            // trying to replace i with updated word
+          word= ("my");}
+      transcript.add(word);
+      
+      if (statement.equals(str)) {  
+      
+        transcript.add(index);
+      
+       
+       
+        
+
+System.out.println(transcript);
+   
+    }}}
+System.out.println("Goodbye!");}}}
+
+    
+  
+  
+
+      
+
+
+
+
+
+
+    
--- reflection.md
+++ reflection.md
@@ -1,7 +1,13 @@
 Use this file to record your reflection on this assignment.
 
-- Which classmates did you consult with while working on this assignment?
-- Which session(s) of TA / office hours did you attend?
-- What are your initial impressions of Java? 
+- Which classmates did you consult with while working on this assignment? Chloe Wahl-Dassule, Ellory
+And these websites (not sure how I am supposed to cite my work)
+https://www.geeksforgeeks.org/randomly-select-items-from-a-list-in-java/ 
+https://www.w3schools.com/java/default.asp
+- Which session(s) of TA / office hours did you attend? Sunday 1-3, Office hours Monday, Office hours tuesday, Ta hours wednesday, I will say it is really difficult to get help because every office and TA hours has been packed with everyone needed help on a different part. 
+
+- What are your initial impressions of Java? I think the syntax is really difficult to get completley right, and I struggled with the automatic compliing, because it made it very hard for me to get to a point where I could even test my code to see if it was doing what I expected.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+Java is definitley more particular about syntax and definitions, Java also feel more disorganized and a bit more all over the place to me right now.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+What worked was reading the error messages closley and googling them if needed, that helped me start to get familiar with Java syntax. What did not work was trying to code too much of my program before thoroughly testing it. I thought my program worked because I did not test enough, then when I went back at the end I realized there were deep problems in my logic, but did not have the time to figure them out because I thought that portion of the code was completed. So advice I would give is to test early and test a lot. 
